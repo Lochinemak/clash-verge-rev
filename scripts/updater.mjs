513a5c54a37144fae8986240d7148576cd43fe74
geneyuriy <yuriygene@veservecompany.com>
2026-08-15 00:51:28 +0800

    chore(updater): use ghfast GitHub proxy

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -285,8 +285,7 @@ async function processRelease(github, options, tag, isAlpha) {
 
     Object.entries(updateDataNew.platforms).forEach(([key, value]) => {
       if (value.url) {
-        updateDataNew.platforms[key].url =
-          `https://update.hwdns.net/${value.url}`
+        updateDataNew.platforms[key].url = `https://ghfast.top/${value.url}`
       } else {
         console.log(`[Error]: updateDataNew.platforms.${key} is null`)
       }
